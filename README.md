f873d237d18cc14923a00de16e265ae95b212daa
pixel-bri <bri.lyn.1995@gmail.com>
2026-07-14 19:06:42 -0400

    Publish Fantasy Big Brother BB28 site

--- README.md
+++
@@ -1,2 +0,0 @@
-# Fantasy-Big-Brother-BB28
-Family BB
---
+++ config.js
@@ -0,0 +1,2 @@
+// Runtime endpoint used by both local builds and static hosting. Keep blank for local-only storage.
+window.FANTASY_BB_API_URL = "";
